fd9e3ae8d1c07d31bfbdf531813e0f8191963b2b
chetan-sharma3 <164554717+chetan-sharma3@users.noreply.github.com>
2024-04-04 18:40:04 +0530

    Update index.js iedi3id

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,13 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json())
-app.use(cors(
-    {
-        origin:["*"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-))
+// app.use(cors(
+//     {
+//         origin:["*"],
+//         methods:["POST","GET"],
+//         credentials:true
+//     }
+// ))
 // const allowedOrigins = ['xproxect-chetans-projects-bed47a5d.vercel.app'];
 // app.use(cors({
 //   origin: function (origin, callback) {
@@ -31,20 +31,20 @@ app.use(cors(
 //     }
 //   }
 // }));
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://xproxect.vercel.app'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://xproxect.vercel.app'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
-app.options('*', (req, res) => { 
-  // Pre-flight request. Reply successfully:
-  res.header('Access-Control-Allow-Origin', 'http://xproxect.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.sendStatus(200);
-});
+// app.options('*', (req, res) => { 
+//   // Pre-flight request. Reply successfully:
+//   res.header('Access-Control-Allow-Origin', 'http://xproxect.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.sendStatus(200);
+// });
 
 mongoose
     .connect(process.env.MONGO_URL, {
