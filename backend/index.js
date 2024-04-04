c48ec264dabbbf61118627152d8041e82f883bed
chetan-sharma3 <164554717+chetan-sharma3@users.noreply.github.com>
2024-04-04 15:50:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,13 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
 
 mongoose
     .connect(process.env.MONGO_URL, {
